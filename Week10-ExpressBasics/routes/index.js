5d3e5e5655d16100c23b65e96115637c30179fa0
Shawn at school <noone@nowhere.net>
2017-11-27 14:46:24 -0800

    modified index.jade

--- Week10-ExpressBasics/routes/index.js
+++ Week10-ExpressBasics/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Prog270' });
+  res.render('index', { title: 'Prog270-Yang' });
 });
 
 router.get('/foo', function(req, res, next) {
